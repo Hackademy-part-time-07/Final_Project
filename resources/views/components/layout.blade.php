957b830921414bbf70db1252139f9a9077b1d8fb
Adrian <adrian.busquets@hotmail.com>
2023-07-03 20:58:37 +0200

    Arreglado 03/07

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }}</title>
-    /*****BOOTSTRAP y
+    {{-- /*****BOOTSTRAP y CSS*****/ --}}
     @vite(['resources/css/app.css'])
+    {{ $style ?? '' }}
 </head>
 <body>
-    
+    <x-nav/>
+    {{ $slot }}
+    {{ $script ?? '' }}
+
+
+
+
+
+
+
+
+    <x-footer/>
+    @vite(['resources/js/app.js'])
 </body>
 </html>
