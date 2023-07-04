1b69d41d015ce46dbbec721c484f80de38aa6f46
Adrian <adrian.busquets@hotmail.com>
2023-07-04 20:15:35 +0200

    Ad.create

---
+++ app/Http/Controllers/AdController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdController extends Controller
+{
+    //
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    public function create(){
+        return view('ad.create');
+    }
+}
---
+++ resources/views/ad/create.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <x-slot name="title">Rapido - Vende algo interesante</x-slot>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        Nuevo anuncio
+                    </div>
+                    <div class="card-body"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
