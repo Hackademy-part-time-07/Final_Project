1c05232d1f90198919a50c87833be7e17fb50601
xsobrevals <xsobrevals@hotmail.es>
2023-07-05 21:34:22 +0200

    cambio

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,12 @@ Route::get('/', [PublicController::class, 'index'])->name('home');
 
 Route::get('/ads/create', [AdController::class,'create'])->name('ads.create');
 
-<<<<<<< HEAD
+
 Route::get('/ads/{ad}', [AdController::class,'show'])->name("ads.show");
 
 
 
-=======
 Route::get('/category/{category}/ads', [PublicController::class, 'adsByCategory'])->name('category.ads');
 
 Route::get('/category/{category->name}/ads', [PublicController::class, 'adsByCategory'])->name('category.ads');
->>>>>>> 4b7a7b32f3e2448cbd49e5e89db652964fa7e309
+
