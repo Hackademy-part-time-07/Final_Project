3e613d971628fae32136a89268bd41b8f35a401a
xsobrevals <xsobrevals@hotmail.es>
2023-07-05 20:46:52 +0200

    primera parte usuari2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdController;
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('welcome');
-});
+Route::get('/', [PublicController::class, 'index'])->name('home');
 
 
 Route::get('/ads/create', [AdController::class,'create'])->name('ads.create');
