3ae8961b6d67866f7314a2b4d7c157a85dcc9ae6
xsobrevals <xsobrevals@hotmail.es>
2023-07-07 19:01:37 +0200

    Register mod

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,10 @@
 <x-layout>
     <!--Register-->
-    <div class="container-fluid bg-accent vh-100">
-    <div class="row mb-5 pb-5">
-        <div class="col-12 col-md-8 ms-5">
-            <div class="d-flex flex-column align-items-center">
-            <div class="form-content justify-content-center mb-5 pb-5">
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-md-6 offset-md-3">
                 <!--FORM TITLE-->
-                <div class="section-title">
-                    <h2 class="form-title space-around"> Crear cuenta
-
-                        <!-- <span> Rapido.es </span> -->
-                    
-
-                    </h2>
-                    <!--<p> Some randmon text </p>-->
-
-                </div>
+                    <h2 class="form-title space-around">Crear cuenta</h2>
                 @if ($errors->any())
                     <div class="alert alert-danger">
                         <ul>
@@ -27,45 +16,39 @@
                 @endif
 
                 <!--FORM FIELDS-->
-                <form action="/register" method="POST" role="form" class="php-email-form">
+                <form action="/register" method="POST" role="form" class="form-control">
                     @csrf
                     <!--Name-->
-                    <div class="form-field-edit form-field space-around my-2">
+                    <div class="space-around my-2">
                         <input type="text" name="name" id="name" class="form-control forms-field-input" placeholder="Tu nombre" data-rule="minlen:4" data-msg="Please enter at least 4 chars"><!--si no vale es _ en forms_field-->
                         <div class="validate"></div>
 
                     </div>
                     <!-- Email-->
-                    <div class="form-field-edit form-field space-around my-2">
+                    <div class="space-around my-2">
                         <input type="email" name="email" id="email" class="form-control forms_field-input" placeholder="Tu correo" data-rule="minlen:4" data-msg="PLease enter at least 4 chars">
                         <div class="validate"></div>
 
                     </div>
                     <!--Password-->
-                    <div class="form-field-edit form-field space-around my-2">
+                    <div class="space-around my-2">
                         <input type="password" name="password" id="password" class="form-control forms_field-input" placeholder="Tu contraseña">
                         <div class="validate"></div>
 
                     </div>
                     <!--Password Confirmation-->
-                    <div class="form-field-edit form-field space-around my-2">
+                    <div class="space-around my-2">
                         <input type="password" name="password_confirmation" id="password_confirmation" class="form-control forms_field-input" placeholder="Tu contraseña otra vez">
                         <div class="validate"></div>
 
                     </div>
                     <!-- Button-Register-->
-                    <button type="submit" class="form-button-edit text-center space-around my-2">
+                    <button type="submit" class="btn btm-info">
                         Crear cuenta
                     </button>
                 </form>
-            </div>
-            <div class="form-link mt-4 d-flex">
-                <p class="text-white">¿Ya eres de los nuestros?</p>
-                <a class="text-reset text-decoration-none ps-2" href="{{ route('login') }}"><u>Entra ya</u></a>
 
-            </div>
-
-        </div>
+                <p class="my-3">¿Ya eres de los nuestros? <a href="{{ route('login') }}" class="btn btn-info btn-sm ms-2">Entra!</a></p>
 
         </div>
 
