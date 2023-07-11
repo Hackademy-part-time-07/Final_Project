065f2f575bac83f1d9a86f6512a5e8f048e7b1c3
xsobrevals <xsobrevals@hotmail.es>
2023-07-11 21:23:53 +0200

    Traduccion 11/07

---
+++ resources/views/components/locale.blade.php
@@ -0,0 +1,5 @@
+<form action="" method="POST">
+    @csrf
+    <button type="submit" class="nav-link px-2" style="background: transparent; border:none;">
+        <span class="flag-icon flag-icon-{{ $country }}"></button>
+</form>
