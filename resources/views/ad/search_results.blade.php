93e3df9690832a3281bb09f55c85e798b35b2552
Eloy <lunadelpinoeloy@gmail.com>
2023-07-26 21:05:14 +0200

    cambios user 9

---
+++ resources/views/ad/search_results.blade.php
@@ -0,0 +1,34 @@
+<x-layout>
+    <x-slot name='title'>{{ $q }}</x-slot> 
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>{{__('Resultados de esta búsqueda') }}: {{ $q}}</h1>
+            </div>
+        </div>
+        <div class="row">
+            @forelse ($ads as $ad)
+            <div class="col-12 col-md-4">
+                <div class="card mb-5 anuncios">
+                    @if ($ad->images()->count() > 0)
+                        <img src="{{ Storage::url($ad->images()->first()->path) }}" class="card-img-top" alt="...">
+                    @else
+                        <img src="{{ !$ad->images()->get()->isEmpty() ? $ad->images()->first()->getUrl(400,300) : 'https://via.placeholder.com/150' }}" alt="..." class="card-img-top">
+                    @endif
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $ad->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{ $ad->price }}</h6>                      
+                        <a href="{{ route('ads.show', $ad) }}" class="btn btn-primary mostrarMas">{{__('Mostrar Más') }}</a>
+                    </div>
+                </div>
+            </div>
+            @empty
+            <div class="col-12">
+                <h2>{{__('Uyy.. parece que no hay nada de esta categoría') }}</h2>
+                <a href="{{ route('ads.create') }}" class="btn btn-success">{{__('Vende tu primer objeto') }}</a> o <a href="{{ route('home') }}" class="btn btn-primary">{{__('Vuelve a la Home') }}</a>
+            </div>
+            @endforelse
+            
+        </div>
+    </div>
+</x-layout>
